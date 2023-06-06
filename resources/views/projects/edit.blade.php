e5d1f1bdca399d4ae15965a0f56d6bbea35b9e35
jasper <j.coenraad11.10@gmail.com>
2023-06-06 15:04:54 +0200

    add project crud with notification

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+    <x-errors :errors="$errors" />
+
+    <div class="card mb-4">
+        <div class="card-header">
+            <h5 class="card-title">{{ $project->title }}</h5>
+        </div>
+
+        <div class="card-body">
+            <form action="{{ route('projects.update', $project) }}" method="POST" class="px-4">
+                @csrf
+                @method('PATCH')
+
+                <div class="mb-3">
+                    <label for="title" class="form-label">Title</label>
+                    <input type="text" id="title" name="title" class="form-control @error('title') is-invalid @enderror" placeholder="Project title" value="{{ old('title', $project->title) }}" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <textarea 
+                        id="description" 
+                        name="description" 
+                        class="form-control @error('description') is-invalid @enderror" 
+                        rows="3">{{ old('description', $project->description) }}</textarea>
+                </div>
+
+                <div class="row gx-3 mb-3">
+                    <div class="col-md-6">
+                        <label for="due_date" class="form-label">Due date</label>
+                        <input type="date" id="due_date" name="due_date" class="form-control @error('due_date') is-invalid @enderror" value="{{ old('due_date', $project->due_date->format('Y-m-d')) }}">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="manager_id" class="form-label">Project manager</label>
+                        <select class="form-select @error('manager_id') is-invalid @enderror" id="manager_id" name="manager_id">
+                            <option value="" selected>Select a project manager</option>
+                            @foreach ($managers as $id => $name)
+                                <option value="{{ $id }}" {{ old('manager_id', $project->manager_id) == $id ? 'selected' : '' }}>{{ $name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit" class="btn btn-primary fw-semibold text-white">
+                        Update
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card mb-4">
+        <div class="card-header">
+            <div class="d-flex justify-content-between align-items-center">
+                <div>
+                    <h3 class="card-title">{{ $project->title }}</h3>
+                </div>
+                <div class="justify-content-end me-2">
+                    <a href="{{ route('projects.edit', $project) }}" class="btn btn-info fw-semibold text-white">
+                        Edit project
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <div class="ms-2">
+                <div class="mb-1">
+                    <p>{{ $project->description }}</p>
+                    <hr />
+                </div>
+                <div class="mb-1">
+                    <h5>Project details</h5>
+                </div>
+                <div class="row gx-1">
+                    <div class="col-1"><span class="fw-bold">Assigned to</span></div>
+                    <div class="col-9"><span>{{ $project->manager ? $project->manager->name : 'Not assigned'}}</span></div>
+                </div>
+                <div class="row gx-1">
+                    <div class="col-1"><span class="fw-bold">Due date</span></div>
+                    <div class="col-9"><span>{{ $project->due_date->format('d M Y') }}</span></div>
+                </div>
+                <hr>
+                <h5>Tasks</h5>
+                <p>No tasks yet.</p>
+            </div>
+        </div>
+    </div>
+@endsection
