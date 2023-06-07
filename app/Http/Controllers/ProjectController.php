e12af59f472da73639d6f3ea35349f08df97c216
jasper <j.coenraad11.10@gmail.com>
2023-06-07 14:44:02 +0200

    fix for ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(Post::class, 'post');
+        $this->authorizeResource(Project::class, 'project');
     }
 
     /**
