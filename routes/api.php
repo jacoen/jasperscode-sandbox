ec2a5290dc3aeda180c28c426085bb9420f5c9fb
jasper <j.coenraad11.10@gmail.com>
2023-08-07 15:28:47 +0200

    updates to api routes

--- routes/api.php
+++ routes/api.php
@@ -22,18 +22,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::resource('/projects', ProjectController::class);
+    Route::apiResource('/projects', ProjectController::class);
     Route::get('/trashed/projects', [ProjectController::class, 'trashed'])->name('projects.trashed');
+    Route::put('/projects/{project}/restore', [ProjectController::class, 'restore'])->withTrashed()->name('projects.restore');
+    
+    Route::apiResource('/tasks', TaskController::class)->except('store');
+    Route::post('/projects/{project}/tasks', [TaskController::class, 'store']);
+    Route::get('/trashed/tasks', [TaskController::class, 'trashed'])->name('tasks.trashed');
+    Route::put('/tasks/{task}/restore', [TaskController::class, 'restore'])->name('tasks.restore');
 
-    Route::get('/projects/{project}/tasks', ProjectTaskController::class)
-        ->middleware('can: read task');
+    Route::get('/projects/{project}/tasks', ProjectTaskController::class);
+    
+    
 
-    Route::controller(TaskController::class)->group(function () {
-        Route::post('/projects/{project}/tasks', 'store');
-
-        Route::prefix('/tasks')->group(function () {
-            Route::get('/', 'index')->name('tasks.index');
-            Route::get('/{task}', 'show')->name('tasks.show');
-        });
-    });
 });
