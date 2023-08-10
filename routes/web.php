a89096c8c2515cc3e75fcbbb69ac9aea9a84d4e2
Jasper <123370769+jacoen@users.noreply.github.com>
2023-08-10 15:15:30 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\TrashedProjectController;
 use App\Http\Controllers\TrashedTaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
