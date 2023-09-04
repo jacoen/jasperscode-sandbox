b4fe55f1658339583d414db5c24f874c25a97149
jasper <j.coenraad11.10@gmail.com>
2023-09-04 13:09:13 +0200

    small update to pagination component

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,12 +1,19 @@
 @props(['records'])
 
+
 <div class="d-flex justify-content-between align-items-center clearfix">
     <div class="pl-2">
-        <span class="fw-semibold">
-            {{ $records->firstItem() }} to {{ $records->lastItem() }} out of {{ $records->total() }} results
-        </span>
+        @if (! $records->count())
+            <span class="fw-semibold">
+                No results
+            </span>
+        @else
+            <span class="fw-semibold">
+                {{ $records->firstItem() }} to {{ $records->lastItem() }} out of {{ $records->total() }} results
+            </span>
+        @endif
     </div>
     <div class="justify-content-start">
-        {{ $records->links() }}
+        {{ $records->withQueryString()->links() }}
     </div>
 </div>
