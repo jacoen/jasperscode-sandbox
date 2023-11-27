026bb199e57bbf836a9ea5083b30954f8453dba3
jasper <j.coenraad11.10@gmail.com>
2023-11-27 20:09:31 +0100

    removed UserObserver

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -11,7 +11,6 @@ use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -33,7 +32,6 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $observers = [
         Task::class => [TaskObserver::class],
-        User::class => [UserObserver::class],
         Project::class => [ProjectObserver::class],
     ];
 
