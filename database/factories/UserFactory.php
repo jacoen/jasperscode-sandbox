8b0f9091aebefa1d1b7b80065d5f9c8cb29b60ff
jasper <j.coenraad11.10@gmail.com>
2023-12-05 13:51:18 +0100

    small refactor for ProjectPolicy and UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'password_token' => null,
+            'password_changed_at' => now(),
         ];
     }
 
@@ -54,6 +56,18 @@ class UserFactory extends Factory
                 'created_at' => now()->subHour(),
                 'updated_at' => now()->subHour(),
                 'token_expires_at' => now()->subHour(),
+                'password_changed_at' => null,
+            ];
+        });
+    }
+
+    public function notActivated()
+    {
+        return $this->state(function (){
+            return [
+                'password_token' => Str::random(32),
+                'token_expires_at' => now()->addHour(),
+                'password_changed_at' => null,
             ];
         });
     }
