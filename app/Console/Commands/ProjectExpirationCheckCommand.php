7023afd6f906ceaebbf3a9487c4038bc4e80bcc2
jasper <j.coenraad11.10@gmail.com>
2024-02-07 12:16:48 +0100

    add command to check for expired projects

---
+++ app/Console/Commands/ProjectExpirationCheckCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Project;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ProjectExpirationCheckCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'project:check-expiration';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check all the active project on expiration';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $count = 0;
+
+        foreach ($this->ActiveProjects() as $project) {
+            $project->timestamps = false;
+            $project->update(['status' => 'expired']);
+            $project->timestamp = true;
+
+            $count++;
+        }
+        
+        Log::info($count.' projects have expired, the status has been changed to expired.');
+        $this->info('The check has been completed. Please check the log for the results');
+        return;
+    }
+
+    private function ActiveProjects()
+    {
+        $projects = Project::whereIn('status', ['open', 'pending', 'restored'])
+            ->where('due_date', '<', now())
+            ->get();
+
+        return $projects;
+    }
+}
