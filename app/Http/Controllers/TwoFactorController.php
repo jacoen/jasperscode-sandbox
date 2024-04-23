6581d2d78f495316c780b546b9ca8a8c6b466223
jasper <j.coenraad11.10@gmail.com>
2024-04-23 15:18:45 +0200

    refactor TwoFactorController

--- app/Http/Controllers/TwoFactorController.php
+++ app/Http/Controllers/TwoFactorController.php
@@ -17,33 +17,25 @@ class TwoFactorController extends Controller
 
     public function store(TwoFactorRequest $request)
     {
-        if ($request->two_factor_code !== auth()->user()->two_factor_code) {
-            $attempts = session()->get('two_factor_attempts', 0);
+        $user = auth()->user();
 
-            $attempts++;
+        $this->resetLoginAttemptsIfNeeded($user);
 
-            session()->put('two_factor_attempts', $attempts);
-            session()->put('last_attempt_time', now()->timestamp);
+        if ($request->two_factor_code !== $user->two_factor_code) {
+            $this->incrementAttempt($user);
 
-
-            if ($attempts > 3 ) {
-                auth()->user()->lockUser();
-                auth()->user()->resetTwoFactorCode();
-                auth()->logout();
-                
-                session()->forget(['two_factor_attempts', 'last_attempt_time']);
-
-                return redirect()->route('login')
-                    ->withErrors(['error' => 'Too many failed attempts. This account will now be locked for a period of 10 minutes.']);
+            if ($user->two_factor_attempts >= 5 ) {
+                return $this->handleExceededAttempts($user);
             }
 
             return redirect()->route('verify.create')->withErrors([
                 'two_factor_code' => 'The two factor code you have entered does not match.',
             ]);
         }
+        
+        $this->resetLoginAttemptData($user);
 
         auth()->user()->resetTwoFactorCode();
-        session()->forget(['two_factor_attempts', 'last_attempt_time']);
 
         return redirect()->route('home');
     }
@@ -57,5 +49,47 @@ class TwoFactorController extends Controller
         return redirect()
             ->route('verify.create')
             ->with('success', 'A new code has been sent to your email.');
+    }
+
+    private function resetLoginAttemptsIfNeeded($user)
+    {
+        if ($user->last_attempt_at && $user->last_attempt_at->lt(now()->subMinutes(5)) && $user->two_factor_attempts >= 1) {
+            $user->timestamps = false;
+            $user->update([
+                'two_factor_attempts' => 0,
+                'last_attempt_at' => null,
+            ]);
+            $user->timestamps = true;
+        }
+    }
+
+    private function incrementAttempt($user)
+    {
+        $user->increment('two_factor_attempts');
+
+        $user->timestamps = false;
+        $user->update(['last_attempt_at' => now()]);
+        $user->timestamps = true;
+    }
+
+    private function handleExceededAttempts($user)
+    {
+        auth()->user()->lockUser();
+        auth()->user()->resetTwoFactorCode();
+        auth()->logout();
+
+        return redirect()->route('login')
+            ->withErrors(['error' => 'Too many failed attempts. This account will now be locked for a period of 10 minutes.']);
+    }
+
+    private function resetLoginAttemptData($user)
+    {
+        if ($user->two_factor_attempts >= 1 && $user->last_attempt_at !== null) {
+            $user->timestamps = false;
+            $user->two_factor_attempts = 0;
+            $user->last_attempt_at = null;
+            $user->save();
+            $user->timestamps = true;
+        }
     }
 }
