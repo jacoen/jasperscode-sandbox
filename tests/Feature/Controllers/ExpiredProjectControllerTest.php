b954d32604c3ade788c1b17accf862a1a69b26ad
jasper <j.coenraad11.10@gmail.com>
2024-04-30 12:44:53 +0200

    refactor tests for ExpiredProjectController

--- tests/Feature/Controllers/ExpiredProjectControllerTest.php
+++ tests/Feature/Controllers/ExpiredProjectControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Controllers;
 
 use App\Models\Project;
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Str;
 use Tests\TestCase;
@@ -42,28 +41,6 @@ class ExpiredProjectControllerTest extends TestCase
                 $this->project->manager->name,
                 'expired',
                 $this->project->due_date->format('d M Y'),
-            ]);
-    }
-
-    public function test_a_user_with_the_read_expired_project_permission_can_filter_the_expired_projects_by_year_and_week()
-    {
-        $manager = User::factory()->create()->assignRole('Manager');
-
-        $projectExpiredOneWeekAgo = Project::factory()->create(['status' => 'expired', 'due_date' => now()->subWeek(), 'manager_id' => $manager->id]);
-        $projectExpiredTwoWeeksAgo = Project::factory()->create(['status' => 'expired', 'due_date' => now()->subWeeks(2), 'manager_id' => $this->manager->id]);
-        $projectExpiredThreeWeeksAgo = Project::factory()->create(['status' => 'expired', 'due_date' => now()->subWeeks(3), 'manager_id' => $manager->id]);
-
-        $yearWeek = now()->year.'-'.now()->subWeeks(2)->weekOfYear; 
-
-        $this->actingAs($this->admin)->get(route('projects.expired', ['yearWeek' => $yearWeek]))
-            ->assertSeeText([
-                Str::limit($projectExpiredTwoWeeksAgo->title, 55),
-                $projectExpiredTwoWeeksAgo->manager->name,
-            ])->assertDontSeeText([
-                $projectExpiredOneWeekAgo->title,
-                $projectExpiredOneWeekAgo->manager->name,
-                $projectExpiredThreeWeeksAgo->title,
-                $projectExpiredThreeWeeksAgo->manager->name,
             ]);
     }
 }
