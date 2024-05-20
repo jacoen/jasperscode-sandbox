e2d811f2cc47350702516b5ec1a6db2cb285b944
jasper <j.coenraad11.10@gmail.com>
2024-05-20 14:51:09 +0200

    oops forgot an exception

--- app/Exceptions/ProjectDeletedException.php
+++ app/Exceptions/ProjectDeletedException.php
@@ -3,8 +3,20 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\Request;
 
 class ProjectDeletedException extends Exception
 {
-    //
+    public function __construct($message)
+    {
+        parent::__construct($message);
+    }
+
+    public function render(Request $request)
+    {
+        return redirect()->route('tasks.trashed')
+            ->withErrors([
+                'error' => $this->getMessage(),
+            ]);
+    }
 }
