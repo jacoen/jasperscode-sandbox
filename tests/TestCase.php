89d93697b9a9b2cb6063f994304d79ee99a18e7d
jasper <j.coenraad11.10@gmail.com>
2024-06-20 14:51:27 +0200

    add a check to the testcase file to make sure the tests work with sqlite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use App\Models\User;
+use Closure;
 use Database\Seeders\PermissionRoleSeeder;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Database\SQLiteConnection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Fluent;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -18,6 +24,12 @@ abstract class TestCase extends BaseTestCase
 
     protected $user;
 
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        $this->resolveSqlite();
+        parent::__construct($name, $data, $dataName);
+    }
+
     public function setUp(): void
     {
         parent::setUp();
@@ -28,5 +40,31 @@ abstract class TestCase extends BaseTestCase
         $this->manager = User::factory()->create()->assignRole('Manager');
         $this->employee = User::factory()->create()->assignRole('Employee');
         $this->user = User::factory()->create()->assignRole('User');
+    }
+
+    public function resolveSqlite()
+    {
+        Connection::resolverFor('sqlite', function ($connection, $database, $prefix, $config) {
+            return new class($connection, $database, $prefix, $config) extends SQLiteConnection {
+                public function getSchemaBuilder()
+                {
+                    if ($this->schemaGrammar === null) {
+                        $this->useDefaultSchemaGrammar();
+                    }
+
+                    return new class($this) extends SQLiteBuilder {
+                        protected function createBlueprint($table, Closure $callback = null)
+                        {
+                            return new class($table, $callback) extends Blueprint {
+                                public function dropForeign($index)
+                                {
+                                    return new Fluent();
+                                }
+                            };
+                        }
+                    };
+                }
+            };
+        });
     }
 }
